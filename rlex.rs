56a56d54be2a61a65d435bd8f7a7decd4723e4de
Corey Richardson <corey@octayn.net>
2014-05-13 12:06:44 -0700

    Update rlex/rparse for upstream changes

--- rlex.rs
+++ rlex.rs
@@ -6,8 +6,7 @@ use syntax::parse::lexer;
 use syntax::parse::lexer::Reader;
 use syntax::parse::token;
 use syntax::ast_util;
-use rustc::driver::driver;
-use rustc::driver::session;
+use rustc::driver::{session, config};
 
 use std::io;
 
@@ -31,7 +30,7 @@ fn token_to_str(tok: token::Token) -> ~str {
             let mut body = StrBuf::from_str(token::get_ident(s).get().to_str());
             if body.as_slice().ends_with(".") {
                 body.push_char('0');  // `10.f` is not a float literal
-            }            
+            }
             format!("LIT_FLOAT({}{})", body, ast_util::float_ty_to_str(t))
         },
         token::LIT_FLOAT_UNSUFFIXED(s) => {
@@ -67,8 +66,8 @@ fn main() {
     for maybe_line in io::stdin().lines() {
         match maybe_line {
             Ok(line) => {
-                let options = session::basic_options();
-                let session = driver::build_session(options, None);
+                let options = config::basic_options();
+                let session = session::build_session(options, None);
                 let filemap = parse::string_to_filemap(&session.parse_sess,
                                                        StrBuf::from_owned_str(line),
                                                        StrBuf::from_str("<n/a>"));
