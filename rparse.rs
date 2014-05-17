aaac618ab887836859ddc87b9c83167fa8f844a1
Peter Green <ptgreen@eml.cc>
2014-05-16 22:16:26 -0700

    Modernize rparse to track trunk libstd.

--- rparse.rs
+++ rparse.rs
@@ -5,6 +5,7 @@ extern crate serialize;
 use std::io;
 use std::io::Reader;
 use std::vec::FromVec;
+use std::strbuf::StrBuf;
 use serialize::json;
 use serialize::json::{Json, List, String, Object};
 
@@ -14,13 +15,13 @@ fn filter_json(j: &mut json::Json) {
     match *j {
         json::Object(ref mut ob) => {
             // remove
-            ob.pop(&"span".to_owned());
-            ob.pop(&"id".to_owned());
-            let mut kv : Option<(~str,~[Json])> = None;
-            match ob.find(&"node".to_owned()) {
+            ob.pop(&StrBuf::from_str("span"));
+            ob.pop(&StrBuf::from_str("id"));
+            let mut kv : Option<(StrBuf,~[Json])> = None;
+            match ob.find(&StrBuf::from_str("node")) {
                 Some(&json::Object(ref ob2)) => {
-                    match (ob2.find(&"variant".to_owned()),
-                           ob2.find(&"fields".to_owned())) {
+                    match (ob2.find(&StrBuf::from_str("variant")),
+                           ob2.find(&StrBuf::from_str("fields"))) {
                         (Some(&String(ref s)), Some(&List(ref ls))) => {
                             kv = Some((s.clone(), FromVec::from_vec(ls.clone())));
                         }
@@ -32,7 +33,7 @@ fn filter_json(j: &mut json::Json) {
             match kv {
                 None => (),
                 Some((k, v)) => {
-                    ob.pop(&"node".to_owned());
+                    ob.pop(&StrBuf::from_str("node"));
                     ob.insert(k, List(Vec::from_slice(v)));
                 }
             }
@@ -55,12 +56,13 @@ fn main() {
             let opt = config::basic_options();
             let sess = session::build_session(opt, None);
             let cfg = config::build_configuration(&sess);
-            let input = driver::StrInput(text);
+            let input = driver::StrInput(StrBuf::from_owned_str(text));
             let cr = driver::phase_1_parse_input(&sess, cfg, &input);
 
             // JSON-ify, meaning "encode then re-parse as just json", ugh.
-            let ast_str = json::Encoder::str_encode(&cr);
-            let mut b = json::Builder::new(ast_str.chars());
+            let ast_str = json::Encoder::str_encode(&cr.module);
+            let s = ast_str.to_str();
+            let mut b = json::Builder::new(s.chars());
             let mut j = b.build().ok().unwrap();
 
             filter_json(&mut j);
